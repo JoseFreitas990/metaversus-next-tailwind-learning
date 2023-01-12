c4b4e1fba7be65546ea330efbef54f2b5532a624
José Bernardo Ferreira de Freitas <1181073@isep.ipp.pt>
2023-01-12 11:56:25 +0000

    NewFeatures Insight World Explore

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,8 +1,18 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+
 const Feedback = () => (
-  <section>
-    Feedback section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    ></motion.div>
   </section>
 );
 
