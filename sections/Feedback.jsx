71f8ec599094d42e4061093aec84f9f9ab8503c7
José Bernardo Ferreira de Freitas <1181073@isep.ipp.pt>
2023-01-12 12:23:34 +0000

    Footer

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -3,6 +3,7 @@
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
+import { fadeIn, staggerContainer, zoomIn } from '../utils/motion';
 
 const Feedback = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -11,8 +12,51 @@ const Feedback = () => (
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto flex flex-col`}
-    ></motion.div>
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-6`}
+    >
+      <motion.div
+        variants={fadeIn('right', 'tween', 0.25, 1)}
+        className="flex-[0.5] lg:max-w-[370px] flex justtify-end flex-col
+        gradient-05 sm:p-8 p-4 rounded-[32px]
+        border-[1px] border-[#6a6a6a] relative"
+      >
+        <div className="feedback-gradient" />
+        <div>
+          <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40px] leading-[36px] text-white">
+            Samantha
+          </h4>
+          <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22px] leading-[16px] text-white">
+            Founder | Metaversus
+          </p>
+        </div>
+        <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45px] leading-[39px] text-white">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam
+          molestiae laudantium officia quam. Corrupti fugit necessitatibus quo
+          delectus fuga, eius nesciunt ipsum in hic consequuntur ratione magnam
+          quas mollitia blanditiis!
+        </p>
+      </motion.div>
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className="relative flex-1 flex justify-center items-center"
+      >
+        <img
+          src="/planet-09.png"
+          alt="planet-09"
+          className="w-full lg:h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]"
+        />
+        <motion.div
+          variants={zoomIn(0.4, 1)}
+          className="lg:block hidden absolute -left-[10%] top-[3%]"
+        >
+          <img
+            src="/stamp.png"
+            alt="stamp"
+            className="w-[155px] h-[155px] object-contain"
+          />
+        </motion.div>
+      </motion.div>
+    </motion.div>
   </section>
 );
 
